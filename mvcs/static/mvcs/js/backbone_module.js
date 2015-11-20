9cc862425ae71de25789e2743e647df99122bd28
Emily Manders <manders.emily@gmail.com>
2015-11-20 14:55:41 -0800

    [backbone] Display list items on the backbone page

--- mvcs/static/mvcs/js/backbone_module.js
+++ mvcs/static/mvcs/js/backbone_module.js
@@ -1,26 +1,55 @@
-var ItemModel = Backbone.Model.extend({
-    defaults: {
-        id: null,
-        name: null,
-        quantity: null,
-        purchased:  null
-    }
-});
-
-var ItemCollection = Backbone.Collection.extend({
-    url: '/items/',
-    model: ItemModel,
-
-    initialize: function() {
-        this.fetch()
-    },
-    parse: function(data) {
-        return data.items;
-    }
-});
-
 $(document).ready(function() {
-    var items = new ItemCollection();
+    var ItemModel = Backbone.Model.extend({
+        defaults: {
+            id: null,
+            name: null,
+            quantity: null,
+            purchased:  null
+        }
+    });
 
-    console.log(items);
+    var ItemCollection = Backbone.Collection.extend({
+        url: '/items/',
+        model: ItemModel,
+
+        initialize: function() {
+            this.fetch()
+        },
+        parse: function(data) {
+            return data.items;
+        }
+    });
+
+    var ItemsView = Backbone.View.extend({
+        el: '.table-body',
+
+        initialize: function() {
+            this.listenTo(this.collection, 'sync', this.render);
+        },
+        render: function() {
+            var $list = this.$('tr.table-item').empty();
+
+            this.collection.each(function(model) {
+                var itemView = new ItemView({model: model});
+                $list.append(itemView.render().$el);
+            }, this);
+
+            return this;
+        }
+    });
+
+    var ItemView = Backbone.View.extend({
+        el: 'tr.table-item',
+        template: _.template($('#item-tmpl').html()),
+
+        render: function() {
+            var html = this.template(this.model.toJSON());
+            this.$el.html(html);
+            return this;
+        }
+
+    });
+
+    var items = new ItemCollection();
+    var itemsView = new ItemsView({collection: items});
 });
